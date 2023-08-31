db19857137afd2c8c853f89362d9c43fea4c4c7b
Gihun Ham <ham@e-mirim.hs.kr>
2023-08-31 14:47:15 +0900

    초기 커밋.

---
+++ DelegatorOperatorPrac31/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatorOperatorPrac31
+{
+
+    internal class Program
+    {
+        public delegate void SendString(string message);
+        static void Main(string[] args)
+        {
+            SendString sayHello, sayGoodbye, multiDelegate;
+            sayHello = Hello;
+            sayGoodbye = GoodBye;
+            multiDelegate = sayHello + sayGoodbye;
+            multiDelegate("아이유");
+            multiDelegate -= sayGoodbye;
+            multiDelegate("아이유");
+        }
+
+        private static void GoodBye(string message)
+        {
+            Console.WriteLine("안녕히가세요, " + message + "양..~");
+        }
+
+        private static void Hello(string message)
+        {
+            Console.WriteLine("안녕하세요, " + message + "양..!");
+        }
+    }
+}
